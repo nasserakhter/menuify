be5d59b8eb3da7ebf1aae834c28769280734f53f
microart-inc <nasrj3@gmail.com>
2022-07-16 11:10:59 -0700

    hi

--- src/io/handleCreateMenu.js
+++ src/io/handleCreateMenu.js
@@ -1,5 +1,40 @@
 import inquirer from "inquirer";
 
 export default async function handleCreateMenu() {
-    // required
+    // required params
+    let name = ""; // display name
+    let description = ""; // description
+    let ext = ""; // the file extension this menu will cover
+    let cascade = false; // whether this is a single button or cascading menu
+    let icon = ""; // the icon to display for this menu
+    let buttons = []; // the buttons in this menu, if cascading
+
+    let params = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter a display name (this will appear in the right click menu):"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Enter a description:"
+        },
+        {
+            type: "input",
+            name: "ext",
+            message: "Enter the file extension this menu will appear on (ex: mp4, mp3, png, etc.):"
+        },
+        {
+            type: "confirm",
+            name: "cascade",
+            message: "Is this a cascading menu (will this be a submenu with multiple buttons [y] or just one single button [n])?",
+            default: false
+        },
+        {
+            type: "input",
+            name: "icon",
+            message: "Enter the icon path to display for this menu (right click ico file and copy the path):"
+        }
+    ]);
 }
