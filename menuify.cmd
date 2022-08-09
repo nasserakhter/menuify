a18473c4d1db5b6057003ea4c9a55cc9173346ff
Microart inc <49543791+microart-inc@users.noreply.github.com>
2022-08-09 13:24:15 -0700

    Update menuify.cmd

--- menuify.cmd
+++ menuify.cmd
@@ -6,19 +6,7 @@ if not "%1"=="am_admin" ("powershell" start -verb runas ""%0"" -argumentList am_
 if not "%2"=="" (
 	cls
 	cd /d "%~dp0"
-	echo Importing assets [1/4] 'coritune.dll'
-	echo Importing assets [2/4] 'windows.dll'
-	echo Importing assets [3/4] 'registry.dll'
-	echo Importing assets [4/4] 'cmd.dll'
-	echo.
-	echo Done.
-	echo Finalizing assets [0/2]
-	echo Finalizing assets [0/2]
-	echo Finalizing assets [1/2]
-	echo Finalizing assets [1/2]
-	echo Finalizing assets [2/2]
-	echo.
-	echo Final launch, this could take a minute...
+	echo If this is your first launch, this could take a minute...
 )
 cd libraries
 launch-platform am_admin
